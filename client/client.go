2d9e28062d87887d35cf1776390b6b3c847967c1
jdxj <985759262@qq.com>
2020-05-21 14:58:36 +0800

    feat: http client 请求限制

--- client/client.go
+++ client/client.go
@@ -1,15 +1,26 @@
 package client
 
 import (
+	"context"
 	"io"
 	"net/http"
 	"net/http/cookiejar"
+	"time"
+
+	"golang.org/x/time/rate"
 )
 
-const userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36"
+const (
+	userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36"
 
-// 全局只使用该 http client
-var client = newClient()
+	interval = 1350 * time.Millisecond
+)
+
+var (
+	// 全局只使用该 http client
+	client  = newClient()
+	limiter = rate.NewLimiter(rate.Every(interval), 1)
+)
 
 func newClient() *http.Client {
 	jar, _ := cookiejar.New(nil)
@@ -32,4 +43,22 @@ func GetReadCloser(url string) (io.ReadCloser, error) {
 		return nil, err
 	}
 	return resp.Body, nil
+}
+
+// LimitedGet 用于限制请求速度
+func LimitedGet(url string) (*http.Response, error) {
+	extent := time.Second
+	timeout := time.Duration(limiter.Limit()*1000+0.5)*time.Millisecond + extent
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	if err := limiter.Wait(ctx); err != nil {
+		return nil, err
+	}
+
+	return Get(url)
+}
+
+func ResetLimiter(interval time.Duration, cap int) {
+	limiter = rate.NewLimiter(rate.Every(interval), cap)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jdxj/wallpaper
 
 go 1.14
 
-require github.com/PuerkitoBio/goquery v1.5.1
+require (
+	github.com/PuerkitoBio/goquery v1.5.1
+	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1
+)
