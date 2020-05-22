34c99b23c0656a433e7d7c327e0a7259dd6ff999
jdxj <985759262@qq.com>
2020-05-22 15:50:34 +0800

    test: download

--- download/download_test.go
+++ download/download_test.go
@@ -1,6 +1,8 @@
 package download
 
 import (
+	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
@@ -30,4 +32,33 @@ func TestNewDownloader(t *testing.T) {
 	_ = d.PushTask(reqTask)
 
 	d.WaitSave()
+}
+
+func TestDownloader_PushTask(t *testing.T) {
+	reqTask := &RequestTask{
+		Path:     "data",
+		FileName: "myPhoto.jpg",
+		URL:      "https://static001.geekbang.org/resource/image/7d/50/7dd3f821b4e790b9367af9f23a7e8750.jpg",
+	}
+
+	d := &Downloader{
+		reqTasks:  make(chan *RequestTask, RequestLimit),
+		saveTasks: make(chan *saveTask, SaveLimit),
+		stop:      make(chan int),
+		giveUp:    make(chan int),
+		reqWG:     &sync.WaitGroup{},
+		saveWG:    &sync.WaitGroup{},
+	}
+	// 测试 PushTask 与 WaitSave 在不同 goroutine 中的执行情况
+	go func() {
+		for i := 0; i < RequestLimit*2; i++ {
+			if err := d.PushTask(reqTask); err != nil { // panic: send on closed channel
+				fmt.Printf("err: %s\n", err)
+				continue
+			}
+		}
+	}()
+
+	time.Sleep(500 * time.Millisecond)
+	close(d.reqTasks)
 }
