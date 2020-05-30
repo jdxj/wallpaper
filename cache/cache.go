632f7ba36da825c2ad919ac4c70b3c1d5422613c
jdxj <985759262@qq.com>
2020-05-30 21:59:57 +0800

    test: 定义缓存方法

--- cache/cache.go
+++ cache/cache.go
@@ -1,73 +1,87 @@
 package cache
 
 import (
-	"database/sql"
+	"encoding/json"
 	"fmt"
 	"time"
+
+	"github.com/dgraph-io/badger/v2"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type storeValue struct {
+	Timestamp int64  `json:"timestamp"`
+	Data      []byte `json:"data"`
+}
+
 const (
 	dbPath = "cache.db"
-
-	// table name
-	Wallhaven = "wallhaven"
-	Octodex   = "octodex"
-	Polayoutu = "polayoutu"
 )
 
 var (
-	sqlite = NewSQLite()
+	DB = NewBadger()
 )
 
-func NewSQLite() *sql.DB {
-	db, err := sql.Open("sqlite3", dbPath)
+func NewBadger() *badger.DB {
+	db, err := badger.Open(badger.DefaultOptions(dbPath))
 	if err != nil {
 		panic(err)
 	}
 	return db
 }
 
-func IsVisited(table, key string) (string, error) {
-	query := fmt.Sprintf("SELECT value,time FROM %s WHERE key=? ORDER BY id DESC LIMIT 1", table)
-	rows, err := sqlite.Query(query, key)
+func IsVisited(key []byte) ([]byte, error) {
+	var result []byte
+	err := DB.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(key)
+		if err != nil {
+			return err
+		}
+
+		result, err = item.ValueCopy(nil)
+		return err
+	})
+
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	if !rows.Next() {
-		rows.Close()
-		return "", fmt.Errorf("key not found")
+	// 检查是否超时
+	sv := &storeValue{}
+	if err := json.Unmarshal(result, sv); err != nil {
+		return nil, err
 	}
-
-	var value string
-	var timestamp int64
-	if err := rows.Scan(&value, &timestamp); err != nil {
-		rows.Close()
-		return "", err
-	}
-	rows.Close()
 
 	// 超过24小时, 缓存失效
-	if time.Now().Unix()-timestamp > 24*60*60 {
-		if err := DeleteValue(table, key); err != nil {
-			fmt.Printf("IsVisited-DeleteValue err: %s\n", err)
+	if time.Now().Unix()-sv.Timestamp > 24*60*60 {
+		if err := DeleteValue(key); err != nil {
+			return nil, err
 		}
-		return "", fmt.Errorf("cache timeout-> key: %s, value: %s",
-			key, value)
+		return nil, fmt.Errorf("this key timeout: %s", key)
 	}
-	return value, nil
+	return sv.Data, nil
 }
 
-func SaveValue(table, key, value string) error {
-	query := fmt.Sprintf("insert into %s (key,value,time) values (?,?,?)", table)
-	_, err := sqlite.Exec(query, key, value, time.Now().Unix())
+func DeleteValue(key []byte) error {
+	err := DB.Update(func(txn *badger.Txn) error {
+		return txn.Delete(key)
+	})
 	return err
 }
 
-func DeleteValue(table, key string) error {
-	query := fmt.Sprintf("DELETE FROM %s WHERE key=?", table)
-	_, err := sqlite.Exec(query, key)
+func SaveValue(key, value []byte) error {
+	sv := &storeValue{
+		Timestamp: time.Now().Unix(),
+		Data:      value,
+	}
+	result, err := json.Marshal(sv)
+	if err != nil {
+		return err
+	}
+
+	err = DB.Update(func(txn *badger.Txn) error {
+		return txn.Set(key, result)
+	})
 	return err
 }
--- cache/cache_test.go
+++ cache/cache_test.go
@@ -5,21 +5,18 @@ import (
 	"testing"
 )
 
-func TestNewSQLite(t *testing.T) {
-	if err := sqlite.Ping(); err != nil {
-		t.Fatalf("%s\n", err)
+func TestIsVisited(t *testing.T) {
+	value, err := IsVisited([]byte("hello"))
+	if err != nil {
+		t.Fatalf("err: %s\n", err)
 	}
-	defer sqlite.Close()
-
-	if err := SaveValue(Wallhaven, "ab", "cd"); err != nil {
-		t.Fatalf("%s\n", err)
-	}
+	fmt.Printf("%s\n", value)
 }
 
-func TestIsVisited(t *testing.T) {
-	visited, err := IsVisited(Wallhaven, "w")
-	if err != nil {
-		t.Fatalf("%s\n", err)
+func TestSaveValue(t *testing.T) {
+	key := []byte("hello")
+	value := []byte("world")
+	if err := SaveValue(key, value); err != nil {
+		t.Fatalf("err: %s\n", err)
 	}
-	fmt.Println(visited)
 }
